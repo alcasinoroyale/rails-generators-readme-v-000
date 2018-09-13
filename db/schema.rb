2f18e7150f9d35aa223edf8257d4fc1edc98eb95
Alex Zdatny <alexzdatny@gmail.com>
2018-09-13 21:42:32 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180913213721) do
+ActiveRecord::Schema.define(version: 20180913214020) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "genre"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
